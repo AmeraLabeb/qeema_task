fc9f8a36eef3fabee4d32037232a185d3000e31d
AmeraLabeb <38465300+AmeraLabeb@users.noreply.github.com>
2022-08-26 05:57:28 +0200

    component ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { StoriesComponent } from './Components/stories/stories.component';
+import { FeedsComponent } from './Components/feeds/feeds.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    StoriesComponent,
+    FeedsComponent
   ],
   imports: [
     BrowserModule,
