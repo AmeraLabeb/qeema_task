d43f2341822e626015f56636c12b5be0da8802cf
AmeraLabeb <38465300+AmeraLabeb@users.noreply.github.com>
2022-08-29 15:32:35 +0200

    style

--- src/app/Components/stories/stories.component.ts
+++ src/app/Components/stories/stories.component.ts
@@ -13,11 +13,11 @@ export class StoriesComponent implements OnInit {
   constructor() { 
       this.stories = [
         {img:'assets/1.webp',name:'you'},
-        {img:'assets/2.webp',name:'mohamed'},
-        {img:'assets/3.webp',name:'fady'},
-        {img:'assets/4.webp',name:'ali'},
-        {img:'assets/5.webp',name:'ahmed'},
-        {img:'assets/6.webp',name:'eslam'},
+        {img:'assets/2.webp',name:'Supardi'},
+        {img:'assets/3.webp',name:'Dean'},
+        {img:'assets/4.webp',name:'Theodore'},
+        {img:'assets/5.webp',name:'Walter'},
+        {img:'assets/6.webp',name:'Jesse'},
         {img:'assets/7.webp',name:'jo'}    
     ]
   this.defaultColor = 'white'
